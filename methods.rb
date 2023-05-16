02eaa7ad6fc1f2c8223a38b04d1a7dd7ebb7e3a5
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-05-16 09:45:38 +0300

    lab done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+  puts ("Hello, programmer!")
+end
+
+def greet(name)
+  puts("Hello, #{name}!")
+end
+puts greet("Naureen")
+
+greet("Feisal")
+
+def greet_with_default(name="programmer")
+  puts("Hello, #{name}!")
+end
+greet_with_default("Naureen")
+greet_with_default()
+
+
+
+def add(num1,num2)
+  return num1 + num2
+end
+add(2,5)
+
+def halve num
+  if num.class == Interger
+    return num/2
+  else puts "Nill"
+  end
+    
+halve(10)
+      
+    
+      
+
